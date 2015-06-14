646f434bfcd882e231c147c9aaee69238776982e
Matthew Coelho <mcoelho@northrock.bm>
2015-06-14 14:51:36 -0300

    starting initial db table creation

--- database/migrations/2015_06_08_213435_setup_initial_tables.php
+++ database/migrations/2015_06_08_213435_setup_initial_tables.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class SetupInitialTables extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -21,6 +21,13 @@ class CreateUsersTable extends Migration {
 			$table->rememberToken();
 			$table->timestamps();
 		});
+
+		Schema::create('password_resets', function(Blueprint $table)
+		{
+			$table->string('email')->index();
+			$table->string('token')->index();
+			$table->timestamp('created_at');
+		});
 	}
 
 	/**
@@ -31,6 +38,6 @@ class CreateUsersTable extends Migration {
 	public function down()
 	{
 		Schema::drop('users');
-	}
+		Schema::drop('password_resets');	}
 
 }
