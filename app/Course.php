b5864621af4bcc4ec51a60d97682bdbf1948a20b
Terry Lin <terrylin1990k@gmail.com>
2015-10-05 17:45:35 -0300

    The model for Course

--- app/Course.php
+++ app/Course.php
@@ -4,6 +4,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model {
 
-	protected $fillable = ['department','name','number','description']
+	protected $fillable = ['department','name','number','description'];
 
 }
